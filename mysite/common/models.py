8f71dcb0038cf78fc1bda16319aa1296eedccc10
abaryshnikov <abaryshnikov@gmail.com>
2015-10-05 10:12:58 -0400

    Initial layout was added

--- mysite/common/models.py
+++ mysite/common/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 class Equipment(models.Model):
 	count = models.IntegerField(default=0)
-    name = models.CharField(max_lengh=255)
-    price = models.DecimalField(default = 0)
+	name = models.CharField(max_length=255)
+	price = models.DecimalField(default = 0, max_digits=2, decimal_places=2)
     
     
 class Brand(models.Model):
-	name = models.CharField(max_lengh = 255)
+	name = models.CharField(max_length = 255)
    		
